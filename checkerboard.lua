d0e71c933f93dcfa0eb9419e287e754a570463ae
Daniel Gomez <dgomez1@mgh.harvard.edu>
2020-10-31 23:28:20 -0400

    Factor out checkerboard functions

---
+++ checkerboard.lua
@@ -0,0 +1,56 @@
+function linspace(start, stop, size)
+   -- Same as linspace in matlab
+   local x = {}
+   local diff = stop - start
+   for i=0, size-1 do
+      x[i+1] = start + i * (diff/(size-1))
+   end
+   return x
+end
+
+function meshgrid(x, y)
+   -- Same as meshgrid in matlab
+   local xx = {}
+   local yy = {}
+
+   for i=1, #y do
+      xx[i] = {}
+      yy[i] = {}
+      for j=1, #x do
+         xx[i][j] = x[j]
+         yy[i][j] = y[i]
+      end
+   end
+   return xx, yy
+
+end
+
+function radial(a, b, spacing_radial)
+   return math.sin((math.sqrt(a*a + b*b)^0.3)*2*math.pi*spacing_radial)
+end
+
+function concentric(a, b, spacing_concentric)
+   return math.sin(math.atan2(a, b) * spacing_concentric)
+end
+
+math.sign = math.sign or function(x) return x<0 and -1 or x>0 and 1 or 0 end
+
+function checkerboard(size_x, size_y, sr, sc)
+   local lx = linspace(-1, 1, size_y)
+   local ly = linspace(-1, 1, size_x)
+   local x, y = meshgrid(lx, ly)
+
+   checks = {}
+   for i, row in ipairs(x) do
+      checks[i] = {}
+      for j, tile in ipairs(row) do
+         local a = x[i][j]
+         local b = y[i][j]
+         local check =  math.sign(radial(a, b, sr)) * math.sign(concentric(a,b, sc))
+         checks[i][j] = math.floor((check + 1) / 2)
+      end
+   end
+
+   return checks
+
+end
--- main.lua
+++ main.lua
@@ -1,63 +1,10 @@
-function linspace(start, stop, size)
-   -- Same as linspace in matlab
-   local x = {}
-   local diff = stop - start
-   for i=0, size-1 do
-      x[i+1] = start + i * (diff/(size-1))
-   end
-   return x
-end
-
--- function print_table(t)
---    print(table.concat(t, ", "))
--- end
-
-function meshgrid(x, y)
-   -- Same as meshgrid in matlab
-   local xx = {}
-   local yy = {}
-
-   for i=1, #y do
-      xx[i] = {}
-      yy[i] = {}
-      for j=1, #x do
-         xx[i][j] = x[j]
-         yy[i][j] = y[i]
-      end
-   end
-   return xx, yy
-
-end
-
-function radial(a, b, spacing_radial)
-   return math.sin((math.sqrt(a*a + b*b)^0.3)*2*math.pi*spacing_radial)
-end
-
-function concentric(a, b, spacing_concentric)
-   return math.sin(math.atan2(a, b) * spacing_concentric)
-end
-
-math.sign = math.sign or function(x) return x<0 and -1 or x>0 and 1 or 0 end
+require("checkerboard")
 
 function love.load()
    width, height = love.graphics.getDimensions()
-   lx = linspace(-1, 1, height)
-   ly = linspace(-1, 1, width)
-   x, y = meshgrid(lx, ly)
-
-   sr = 10
-   sc = 15
-
-   checks = {}
-   for i, row in ipairs(x) do
-      checks[i] = {}
-      for j, tile in ipairs(row) do
-         a = x[i][j]
-         b = y[i][j]
-         check =  math.sign(radial(a, b, sr)) * math.sign(concentric(a,b, sc))
-         checks[i][j] = math.floor((check + 1) / 2)
-      end
-   end
+   sr = 10 -- radial spacing
+   sc = 15 -- concentric spacing
+   checks = checkerboard(width, height, sr, sc)
 
    --Create a table named colors
     colors_forward = {
